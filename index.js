30b4be0f961453a42d301f6e44d312b4ff1c6696
artem1214 <61391756+artemanoh@users.noreply.github.com>
2025-04-04 11:15:22 +0300

    make hm

---
+++ index.js
@@ -0,0 +1,16 @@
+// Task 1
+const sliderInput = document.querySelector(".slider__input");
+const sliderImage = document.querySelector(".slider__image")
+
+sliderInput.addEventListener('input', _.debounce((event) => {
+sliderImage.style.width = `${sliderInput.value * 4}px`
+}, 300)
+);
+// Task 2
+const movingBox = document.querySelector('#box')
+
+window.addEventListener('mousemove', _.debounce((event) => {
+movingBox.style.left = event.clientX + "px"
+movingBox.style.top =  event.clientY + "px"
+}, 100)
+);
